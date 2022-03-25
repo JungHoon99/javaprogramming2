ff6a74962f9491234d5a67e3096b263d84700772
JungHoon99 <44799091+JungHoon99@users.noreply.github.com>
2022-03-25 20:15:57 +0900

    Update SocketServer.java

--- seerver/SocketServer.java
+++ seerver/SocketServer.java
@@ -4,14 +4,15 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 
 public class SocketServer {
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, SQLException {
 		int port=8888;
-		
+		database db=new database();
 		ServerSocket socket = new ServerSocket(port);
 		System.out.println("접속대기중");
 		
@@ -20,15 +21,13 @@ public class SocketServer {
 			
 			System.out.println("사용자가 접속했습니다.");
 			System.out.println("Client : "+sock.getInetAddress());
-			
 			OutputStream out = sock.getOutputStream();
 			DataOutputStream dous = new DataOutputStream(out);
-			
 			dous.writeUTF("환영합니다.");
 			dous.flush();
 			dous.close();
-
 		}
+		
 	}
 	
 }
