27a14224542ca82b2ec4d975d112c5893dd371b1
JungHoon99 <44799091+JungHoon99@users.noreply.github.com>
2022-03-25 22:57:08 +0900

    Create mainMenu.java

---
+++ project/mainMenu.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+/*
+ * 티켓팅 프로그램의 메인메뉴
+ */
+public class mainMenu extends JFrame{
+	public mainMenu() {
+		setTitle("티켓모아");
+		setLayout(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		setSize(1200,700);
+		setVisible(true);
+	}
+}
