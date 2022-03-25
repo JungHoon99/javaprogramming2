831286318b671b67a371164f39d7cb0da0a55d41
JungHoon99 <44799091+JungHoon99@users.noreply.github.com>
2022-03-25 20:15:28 +0900

    database select

--- seerver/database.java
+++ seerver/database.java
@@ -1,18 +1,27 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class database {
-	public database() {
+	Statement st;
+	public database() throws SQLException {
 		String user = "root";
 		String password = "kmk20216*";
-		String url = "jdbc:mysql://localhost:3306";
-		try(
-			Connection conn = DriverManager.getConnection(url, user, password);
-		) {
-			System.out.println(conn.toString());
-		} catch (SQLException e) {
-			e.printStackTrace();
+		String url = "jdbc:mysql://localhost:3306/ticketmoa";
+		Connection conn = DriverManager.getConnection(url, user, password);
+		
+		st = conn.createStatement();
+		System.out.println(conn.toString());
+	}
+	
+	public void select() throws SQLException {
+		String str = "Select * from user";
+		ResultSet query = st.executeQuery(str+";");
+		
+		while(query.next()) {
+			System.out.println(query.getString(2));
 		}
 	}
 }
