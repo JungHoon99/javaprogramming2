1b4e7ee846d640724823bf494953c63764ae8135
JungHoon99 <44799091+JungHoon99@users.noreply.github.com>
2022-03-26 20:19:22 +0900

    add database

--- seerver/SocketServer.java
+++ seerver/SocketServer.java
@@ -1,3 +1,13 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+
 public class SocketServer  {
 	
 	public static void main(String[] args) throws IOException, SQLException {
@@ -23,6 +33,7 @@ class ReceiveThread extends Thread {
 
 	  private final Socket socket;
 	  private String receiveString;
+	  private String getData;
 	  database db;
 
 	  public ReceiveThread(Socket socket, database db) {
@@ -34,21 +45,70 @@ class ReceiveThread extends Thread {
 	  public void run() {
 	    try {
 	      DataInputStream tmpbuf = new DataInputStream(socket.getInputStream());
+	      DataOutputStream sendWriter = new DataOutputStream(socket.getOutputStream());
 	      while (true) {
 	        receiveString = tmpbuf.readUTF();
 	        if (receiveString == null) {
 	          System.out.println("상대방 연결이 종료되었습니다.");
 	        } else {
 	          System.out.println("상대방 : " + receiveString);
-	          db.count(receiveString);
+	          getData = db.count(receiveString);
+	          System.out.println(getData);
+	          
 	        }
+			sendWriter.writeUTF(getData);
+			sendWriter.flush();
 	      }
 	    } catch (IOException e) {
 	      e.printStackTrace();
 	    } catch (SQLException e) {
 			e.printStackTrace();
 		}
+	  }
+}
 
+
+/*
+public class SocketServer {
+	 public static void main(String[] args) throws IOException {
+		    int port = 8888;
+		    ServerSocket socketServer = new ServerSocket(port);
+		    System.out.println("서버가 시작되었습니다.");
+		    Socket sock = socketServer.accept();
+		    System.out.println("ip : " + sock.getInetAddress() + " 와 연결되었습니다.");
+		    while(true) {
+		      Socket sock = socketServer.accept();
+		      System.out.println("ip : " + sock.getInetAddress() + " 와 연결되었습니다.");
+		      ReceiveThread receiveThread = new ReceiveThread(sock);
+		      receiveThread.start();
+		      SendThread sendThread = new SendThread(sock);
+		      sendThread.start();
+		    }
+		  }
+}
+
+class SendThread extends Thread{
+
+	  private final Socket socket;
+	  private Scanner scanner = new Scanner(System.in);
+
+	  public SendThread(Socket socket){
+	    this.socket = socket;
 	  }
 
-}
+	  @Override
+	  public void run() {
+	    try {
+	      DataOutputStream sendWriter = new DataOutputStream(socket.getOutputStream());
+	      String sendString;
+	      while(true){
+	        sendString = scanner.nextLine();
+	        sendWriter.writeUTF(sendString);
+	        sendWriter.flush();
+	      }
+	    }catch (IOException e){
+	      e.printStackTrace();
+	    }
+	  }
+	}
+*/
