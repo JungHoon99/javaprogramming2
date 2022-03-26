0a29f5cf09fbe5717d6a8182c4b7f756644e192e
JungHoon99 <44799091+JungHoon99@users.noreply.github.com>
2022-03-26 16:50:28 +0900

    Update ticketClient.java

--- project/ticketClient.java
+++ project/ticketClient.java
@@ -1,9 +1,11 @@
 package ticketMoa;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ticketClient extends Thread{
 	
@@ -13,12 +15,15 @@ public class ticketClient extends Thread{
 	public ticketClient() throws IOException{
 		socket = new Socket("127.0.0.1",8888);
 		System.out.println("서버와 접속되었습니다.");
-		InputStream ins = socket.getInputStream();
-		DataInputStream dins = new DataInputStream(ins);
-		
-		str = dins.readUTF();
 	  }
-	public String getString() {
-		return str;
+	
+	public void select(String get) {
+		try {
+			DataOutputStream sendWriter = new DataOutputStream(socket.getOutputStream());
+			sendWriter.writeUTF(get);
+			sendWriter.flush();
+		    }catch (IOException e){
+		      e.printStackTrace();
+		    }
 	}
 }
